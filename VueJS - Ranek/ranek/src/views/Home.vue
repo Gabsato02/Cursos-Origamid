456126505ba4dde3f915b91a1799e84bec988f66
Gabriel Sato <gabriel.sato@empiricus.com.br>
2020-12-27 12:22:34 -0300

    Curso de Vue JS - Projeto Ranek

---
+++ VueJS - Ranek/ranek/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h1>👌 Compre ou Venda 🤏</h1>
+    <produtos-buscar></produtos-buscar>
+    <produtos-lista></produtos-lista>
+  </section>
+</template>
+
+<script>
+
+import ProdutosBuscar from '../components/ProdutosBuscar.vue';
+import ProdutosLista from '../components/ProdutosLista.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    ProdutosLista,
+    ProdutosBuscar
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 2rem;
+    color: #87f;
+    margin-top: 40px;
+    text-align: center;
+  }
+
+</style>
